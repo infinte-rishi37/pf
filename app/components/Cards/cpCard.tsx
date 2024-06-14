30a3be3648abb8d954f303269078d34ce8ca38b4
Rishikumar7137 <52399450+Rishikumar7137@users.noreply.github.com>
2024-06-15 04:00:25 +0530

    second commit

--- app/components/Cards/cpCard.tsx
+++ app/components/Cards/cpCard.tsx
@@ -1,4 +1,4 @@
-import getData from '../../api/getData';
+// import getData from '../../api/getData';
 import { useState } from 'react';
 
 const CPCard = () => {
@@ -8,24 +8,24 @@ const CPCard = () => {
     const [info, setInfo] = useState({});
     const [handle, setHandle] = useState({});
     
-    let prom = getData({url: reqUrlInfo});
+    // let prom = getData({url: reqUrlInfo});
     
-    prom
-    .then((data) => {
-        setInfo(data.result[0]);
-    })
-    .catch((error) => {
-        console.error("Error fetching data:", error);
-    });
+    // prom
+    // .then((data) => {
+    //     setInfo(data.result[0]);
+    // })
+    // .catch((error) => {
+    //     console.error("Error fetching data:", error);
+    // });
 
-    prom = getData({url: reqUrlHandle});
-    prom
-    .then((data) => {
-        setHandle(data.result[0]);
-    })
-    .catch((error) => {
-        console.error("Error fetching data:", error);
-    });
+    // prom = getData({url: reqUrlHandle});
+    // prom
+    // .then((data) => {
+    //     setHandle(data.result[0]);
+    // })
+    // .catch((error) => {
+    //     console.error("Error fetching data:", error);
+    // });
     
     return (
         <main className='bg-pall-dl bg-opacity-25 p-6 m-2'>
@@ -41,7 +41,7 @@ const CPCard = () => {
                         Object.keys(info).map((key) => {
                             return (
                                 <div key={key}>
-                                    {key} : {info[key]}
+                                    {/* {key} : {info[key]} */}
                                 </div>
                             )
                         })
